f3001eec04dafffef56f3f8cc0a4f39738ad2243
kmgyu <lidersy961@gmail.com>
2025-03-23 11:30:49 +0000

    [feat] field management - create 기능 추가

--- fieldmanage/models.py
+++ fieldmanage/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from djongo import models as djongo_models
 
-# Create your models here.
+class Field(models.Model):
+    field_name = models.CharField(max_length=100)
+    field_address = models.CharField(max_length=255)
+    field_area = models.FloatField()
+    crop_name = models.CharField(max_length=100)
+    farm_startdate = models.DateField()
+
+    geometry = djongo_models.JSONField()  # GeoJSON 저장용
+
+    def __str__(self):
+        return self.field_name
