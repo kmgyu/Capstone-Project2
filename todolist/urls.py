c78805484a0660934b67296f523c940dfff6fa7c
kmgyu <lidersy961@gmail.com>
2025-03-30 11:13:22 +0000

    [feat] todo list - todo list CRUD feature added

---
+++ todolist/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import FieldTodoListView, FieldTodoDetailView
+
+urlpatterns = [
+    path('todos/', FieldTodoListView.as_view(), name='todo-list'),
+    path('todos/<int:pk>/', FieldTodoDetailView.as_view(), name='todo-detail'),
+    path('todos/create/', FieldTodoDetailView.as_view(), name='todo-create'),
+]
