c45a0d82c83a3a0d2eee636238a96589f3490d3a
kmgyu <lidersy961@gmail.com>
2025-04-09 02:00:14 +0000

    [feat] 토큰 재발급 기능 추가

--- fieldmanage/urls.py
+++ fieldmanage/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from .views import FieldListView, FieldDetailView
+from rest_framework_simplejwt.views import TokenRefreshView
 
 # ~/field
 urlpatterns = [
     path('fields/', FieldListView.as_view(), name='field-list'),
     path('fields/<int:pk>/', FieldDetailView.as_view(), name='field-detail'),
+    path("refresh/", TokenRefreshView.as_view()), # jwt 토큰 재발급
 ]
