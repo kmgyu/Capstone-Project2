37995017150e8c1830ce6f1ed06db7def8c6f869
kmgyu <lidersy961@gmail.com>
2025-04-09 02:24:31 +0000

    [chore] 토큰 재발급 url 수정

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 # from .views import LoginView, LogoutView, RegisterView
 from .views import AuthAPIView, RegisterAPIView
+from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
     # post - login, delete - logout, get - user info
     path('auth/', AuthAPIView.as_view(), name='auth'),
     path('register/', RegisterAPIView.as_view(), name='register'),
+    path("refresh/", TokenRefreshView.as_view()), # jwt 토큰 재발급
 ]
