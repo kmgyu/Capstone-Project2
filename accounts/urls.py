37df53fe5d9738a0babc068254e3a380eb79226a
kmgyu <lidersy961@gmail.com>
2025-04-15 14:06:24 +0000

    [route] 라우팅 수정 - append_slash 추가 - 파일 처리 url 추가

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,10 +6,10 @@ from .views import ForgotPasswordView, ResetPasswordVerifyView, ResetPasswordVie
 
 urlpatterns = [
     # post - login, delete - logout, get - user info
-    path('auth', AuthAPIView.as_view(), name='auth'),
-    path('register', RegisterAPIView.as_view(), name='register'),
-    path("refresh", TokenRefreshView.as_view()), # jwt 토큰 재발급
-    path("forgot-password", ForgotPasswordView.as_view(), name="forgot-password"),
-    path("reset-password/<uuid:token>/verify", ResetPasswordVerifyView.as_view(), name="reset-password-verify"),
-    path("reset-password/<uuid:token>", ResetPasswordView.as_view(), name="reset-password"),
+    path('auth/', AuthAPIView.as_view(), name='auth'),
+    path('register/', RegisterAPIView.as_view(), name='register'),
+    path("refresh/", TokenRefreshView.as_view()), # jwt 토큰 재발급
+    path("forgot-password/", ForgotPasswordView.as_view(), name="forgot-password"),
+    path("reset-password/<uuid:token>/verify/", ResetPasswordVerifyView.as_view(), name="reset-password-verify"),
+    path("reset-password/<uuid:token>/", ResetPasswordView.as_view(), name="reset-password"),
 ]
