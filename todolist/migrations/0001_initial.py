85c9411c31a85e25a375f9a67276b8e7d0e0ca26
kmgyu <lidersy961@gmail.com>
2025-04-15 14:05:32 +0000

    [DB] migration 및 columns 수정 - 컬럼 마이그레이션 중복 에러 수정 - datetimefield 기본 값 생성 수정

--- todolist/migrations/0001_initial.py
+++ todolist/migrations/0001_initial.py
@@ -2,6 +2,7 @@
 
 import django.db.models.deletion
 from django.db import migrations, models
+from django.conf import settings
 
 
 class Migration(migrations.Migration):
@@ -24,6 +25,7 @@ class Migration(migrations.Migration):
                 ('period', models.IntegerField(blank=True, null=True)),
                 ('is_tmp', models.BooleanField(default=False)),
                 ('field', models.ForeignKey(db_column='field_id', on_delete=django.db.models.deletion.CASCADE, to='fieldmanage.field')),
+                ('owner', models.ForeignKey( db_column='owner_id', on_delete=django.db.models.deletion.CASCADE, related_name='todos', to=settings.AUTH_USER_MODEL)),
             ],
             options={
                 'db_table': 'field_todo',
