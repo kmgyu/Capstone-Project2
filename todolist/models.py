2bc99dc403bf8d4ae8b13e296ffe3e8a49d4dea8
kmgyu <lidersy961@gmail.com>
2025-04-15 14:05:32 +0000

    [DB] migration 및 columns 수정 - 컬럼 마이그레이션 중복 에러 수정 - datetimefield 기본 값 생성 수정

--- todolist/models.py
+++ todolist/models.py
@@ -7,12 +7,12 @@ User = get_user_model()
 
 class FieldTodo(models.Model):
     task_id = models.AutoField(primary_key=True)
-    owner = models.ForeignKey("accounts.User", on_delete=models.CASCADE, related_name='todos')
+    owner = models.ForeignKey("accounts.User", on_delete=models.CASCADE, related_name='todos', db_column='owner_id')
     field = models.ForeignKey(Field, on_delete=models.CASCADE, db_column='field_id')  # 어떤 노지의 할 일인지
     task_name = models.CharField(max_length=255)
     task_content = models.TextField(blank=True, null=True)
     cycle = models.IntegerField(blank=True, null=True)
-    start_date = models.DateTimeField(blank=True, null=True)
+    start_date = models.DateTimeField(auto_now_add=True)
     period = models.IntegerField(blank=True, null=True)
     is_tmp = models.BooleanField(default=False)
 
