103944d902f4100e051319db6beeaa32ac704973
kimSummerrain <doeun457@gmail.com>
2025-04-17 23:38:10 +0900

    [feat]멀티파트 기능 구현

--- multipart_picture/models.py
+++ multipart_picture/models.py
@@ -20,3 +20,20 @@ class PestPic(models.Model):
     pic_name = models.CharField(max_length=255)
     pic_path = models.ImageField(upload_to='pest/')
     pic_time = models.DateTimeField(auto_now_add=True)
+
+#업로드 사진 경로
+#MEDIA_URL = '/media/'
+#MEDIA_ROOT = '/home/Capstone-Project2/storage'
+
+
+#config안의 urls.py에 추가할 파일
+#from django.conf import settings
+#from django.conf.urls.static import static
+
+# urlpatterns = [
+#     path("admin/", admin.site.urls),
+#     path("auth/", include("accounts.urls")),
+#     path("field/", include("fieldmanage.urls")),
+#     path("todo/", include("todolist.urls")),
+#     path("upload/", include("multipart_picture.urls")),  # 추가된 이미지 업로드 경로
+# ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- multipart_picture/views.py
+++ multipart_picture/views.py
@@ -1,33 +1,55 @@
-from django.shortcuts import render, redirect
-from .forms import FieldPicForm, CropPicForm, PestPicForm
-from .models import FieldPic, CropPic, PestPic
+# multipart_picture/views.py
 
-def upload_field(request):
+from django.http import JsonResponse
+from .forms import FieldPicForm, CropPicForm, PestPicForm
+
+def upload_field_pic(request):
     if request.method == 'POST':
         form = FieldPicForm(request.POST, request.FILES)
         if form.is_valid():
-            form.save()
-            return redirect('upload_field')
-    else:
-        form = FieldPicForm()
-    return render(request, 'fieldmanage/upload_field.html', {'form': form})
+            instance = form.save()
+            return JsonResponse({
+                'status': 'success',
+                'message': 'FieldPic uploaded successfully',
+                'data': {
+                    'id': instance.field_pic_id,
+                    'pic_name': instance.pic_name,
+                    'pic_path': instance.pic_path.url
+                }
+            })
+        else:
+            return JsonResponse({'status': 'error', 'errors': form.errors}, status=400)
 
-def upload_crop(request):
+def upload_crop_pic(request):
     if request.method == 'POST':
         form = CropPicForm(request.POST, request.FILES)
         if form.is_valid():
-            form.save()
-            return redirect('upload_crop')
-    else:
-        form = CropPicForm()
-    return render(request, 'fieldmanage/upload_crop.html', {'form': form})
+            instance = form.save()
+            return JsonResponse({
+                'status': 'success',
+                'message': 'CropPic uploaded successfully',
+                'data': {
+                    'id': instance.crop_pic_id,
+                    'pic_name': instance.pic_name,
+                    'pic_path': instance.pic_path.url
+                }
+            })
+        else:
+            return JsonResponse({'status': 'error', 'errors': form.errors}, status=400)
 
-def upload_pest(request):
+def upload_pest_pic(request):
     if request.method == 'POST':
         form = PestPicForm(request.POST, request.FILES)
         if form.is_valid():
-            form.save()
-            return redirect('upload_pest')
-    else:
-        form = PestPicForm()
-    return render(request, 'fieldmanage/upload_pest.html', {'form': form})
+            instance = form.save()
+            return JsonResponse({
+                'status': 'success',
+                'message': 'PestPic uploaded successfully',
+                'data': {
+                    'id': instance.pest_pic_id,
+                    'pic_name': instance.pic_name,
+                    'pic_path': instance.pic_path.url
+                }
+            })
+        else:
+            return JsonResponse({'status': 'error', 'errors': form.errors}, status=400)
