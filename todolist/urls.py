436a2c9feb7bb7edd234e5139e20a661f4166bfa
kmgyu <lidersy961@gmail.com>
2025-04-23 14:54:56 +0000

    [refactor] todolist 기능 조회 및 생성 기능 통합 - 클래스 통합 및 endpoint 수정

--- todolist/urls.py
+++ todolist/urls.py
@@ -7,13 +7,10 @@ from .views import (
 from .tasks import create_task_from_gpt
 
 urlpatterns = [
-    # 사용자 기준 start~end date 할 일 목록 조회
+    # 사용자 기준 start~end date 할 일 목록 조회 및 생성
     # field_id 존재 시, field_id 또한 필터링 옵션으로 들어감
     path('todos/list/', FieldTodoListAPIView.as_view(), name='todo-list'),
     path('todos/<int:field_id>/list/', FieldTodoListAPIView.as_view(), name='todo-list-by-field'),
-
-    # 특정 필드의 할 일 목록 조회 및 생성
-    path('todos/field/<int:field_id>/', FieldTodoListCreateAPIView.as_view(), name='todo-field-list-create'),
 
     # 특정 할 일 한 건 조회/수정/삭제
     path('todos/task/<int:task_id>/', FieldTodoDetailAPIView.as_view(), name='todo-detail'),
