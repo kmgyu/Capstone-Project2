5ab4b5fdd376ba3c8a2613e3bc1b9e5c19a64564
kmgyu <lidersy961@gmail.com>
2025-05-09 08:41:16 +0000

    [update] crop,pest model,view 부분을 주석 처리함

--- multipart_picture/models.py
+++ multipart_picture/models.py
@@ -10,16 +10,16 @@ class FieldPic(models.Model):
     longitude = models.FloatField(null=True, blank=True)
     pic_time = models.DateTimeField(auto_now_add=True)
 
-class CropPic(models.Model):
-    crop_pic_id = models.AutoField(primary_key=True)
-    field_pic = models.ForeignKey(FieldPic, on_delete=models.CASCADE)
-    pic_name = models.CharField(max_length=255)
-    pic_path = models.ImageField(upload_to='crop/')
-    pic_time = models.DateTimeField(auto_now_add=True)
+# class CropPic(models.Model):
+#     crop_pic_id = models.AutoField(primary_key=True)
+#     field_pic = models.ForeignKey(FieldPic, on_delete=models.CASCADE)
+#     pic_name = models.CharField(max_length=255)
+#     pic_path = models.ImageField(upload_to='crop/')
+#     pic_time = models.DateTimeField(auto_now_add=True)
 
-class PestPic(models.Model):
-    pest_pic_id = models.AutoField(primary_key=True)
-    crop_pic = models.ForeignKey(CropPic, on_delete=models.CASCADE)
-    pic_name = models.CharField(max_length=255)
-    pic_path = models.ImageField(upload_to='pest/')
-    pic_time = models.DateTimeField(auto_now_add=True)
+# class PestPic(models.Model):
+#     pest_pic_id = models.AutoField(primary_key=True)
+#     crop_pic = models.ForeignKey(CropPic, on_delete=models.CASCADE)
+#     pic_name = models.CharField(max_length=255)
+#     pic_path = models.ImageField(upload_to='pest/')
+#     pic_time = models.DateTimeField(auto_now_add=True)
