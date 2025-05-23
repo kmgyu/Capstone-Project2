44201ed514c481b9a8db64c3858b7093697d9d2f
myomyeo <qudgus001212@gmail.com>
2025-05-23 12:54:37 +0000

    [feat] 사진 업로드 API 수정 - 토큰 접근을 field_id로 접근하는 방식으로 수정

--- photoapp/serializers.py
+++ photoapp/serializers.py
@@ -2,6 +2,10 @@ from rest_framework import serializers
 from .models import FieldPic
 
 class FieldPicSerializer(serializers.ModelSerializer):
+    field_id = serializers.IntegerField(write_only=True)
     class Meta:
         model = FieldPic
-        fields = ['pic_name', 'pic_path']
+        
+        fields = ['field_id','pic_name', 'pic_path']
+        
+    pic_path = serializers.ImageField(required=True)
