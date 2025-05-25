f8a82fa1ea771fb1787a13e76e332fc5347ab355
myomyeo <qudgus001212@gmail.com>
2025-05-25 14:21:08 +0000

    [REFACTOR] has_pest, has_disease 필드 및 FlaskResultUpdateAPIView 삭제

--- photoapp/models.py
+++ photoapp/models.py
@@ -7,14 +7,11 @@ class FieldPic(models.Model):
     field = models.ForeignKey(Field, on_delete=models.CASCADE)    
     pic_name = models.CharField(max_length=255)
     pic_path = models.CharField(max_length=300, blank=True)
+    
     #metadata로 받는 데이터(위도,경도,찍은시간)
     latitude = models.FloatField(null=True)
     longitude = models.FloatField(null=True)
     pic_time = models.DateTimeField(null=True)
-
-    # Flask에서 분석 결과 반영할 필드
-    has_pest = models.BooleanField(null=True)
-    has_disease = models.BooleanField(null=True)
 
     class Meta:
         db_table = "photoapp_fieldpic"
